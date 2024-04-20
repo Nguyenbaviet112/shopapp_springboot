236b26f6ce4d815c199f3df4d8b281ade5b5a4e4
NguyenBaViet <nguyenbaviet112@gmail.com>
2024-04-20 21:15:42 +0700

    update models: User.java

--- shoappp_backend/shopapp/src/main/java/com/project/shopapp/configurations/SecurityConfig.java
+++ shoappp_backend/shopapp/src/main/java/com/project/shopapp/configurations/SecurityConfig.java
@@ -1,4 +1,29 @@
 package com.project.shopapp.configurations;
 
+import com.project.shopapp.models.User;
+import com.project.shopapp.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserRepository userRepository;
+
+    //user's detail object
+    public UserDetailsService userDetailsService() {
+
+        return phoneNumber -> {
+            User existingUser = userRepository
+                    .findByPhoneNumber(phoneNumber)
+                    .orElseThrow(()  -> new UsernameNotFoundException("Cannot find user with phone number " + phoneNumber));
+
+            return existingUser;
+        }
+
+    }
+
 }
