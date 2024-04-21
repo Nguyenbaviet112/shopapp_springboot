61c3b316ef150d8596e28b9a90201bd8e15ab347
NguyenBaViet <nguyenbaviet112@gmail.com>
2024-04-21 22:54:01 +0700

    update jwtTokenUtil

--- shoappp_backend/shopapp/src/main/java/com/project/shopapp/components/jwtTokenUtil.java
+++ shoappp_backend/shopapp/src/main/java/com/project/shopapp/components/jwtTokenUtil.java
@@ -1,13 +1,18 @@
 package com.project.shopapp.components;
 
 import com.project.shopapp.models.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.*;
+import java.util.function.Function;
 
 @Component
 @RequiredArgsConstructor
@@ -24,19 +29,45 @@ public class jwtTokenUtil {
         claims.put("phoneNumber", user.getPhoneNumber());
         try {
             String token = Jwts.builder()
-                    .setClaims(claims)
+                    .setClaims(claims) // how to extract claims from this?
                     .setSubject(user.getPhoneNumber())
                     .setExpiration(new Date(System.currentTimeMillis() + expiration * 1000L))
-                    .signWith()
+                    .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                     .compact();
+
+            return token;
         }
         catch (Exception e) {
+            // you can "inject" Logger, instead System.out.println
+            System.out.println("Cannot create JWT token, error: " + e.getMessage());
+            return null;
+
 
         }
     }
 
     private Key getSignInKey() {
+        byte[] bytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(bytes);
+    }
 
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    // Check expiration
+    public boolean isTokenExpired(String token) {
+        Date expiration = this.extractClaim(token, Claims::getExpiration);
+        return expiration.before(new Date());
     }
 
 }
