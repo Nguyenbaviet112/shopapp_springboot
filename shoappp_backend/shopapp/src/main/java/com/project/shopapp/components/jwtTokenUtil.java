4eb1a1fb7f93f764e7ddf65f3a0c2b1bc99b4688
NguyenBaViet <nguyenbaviet112@gmail.com>
2024-04-21 22:37:19 +0700

    add variable environment in application.yml

--- shoappp_backend/shopapp/src/main/java/com/project/shopapp/components/jwtTokenUtil.java
+++ shoappp_backend/shopapp/src/main/java/com/project/shopapp/components/jwtTokenUtil.java
@@ -1,7 +1,42 @@
 package com.project.shopapp.components;
 
+import com.project.shopapp.models.User;
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
+import java.util.*;
+
 @Component
+@RequiredArgsConstructor
 public class jwtTokenUtil {
+
+    @Value("${jwt.expiration}")
+    private int expiration; // save to an environment variable
+    @Value("${jwt.secretKey}")
+    private String secretKey;
+
+    public String generateToken(com.project.shopapp.models.User user) {
+        // properties => claims
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("phoneNumber", user.getPhoneNumber());
+        try {
+            String token = Jwts.builder()
+                    .setClaims(claims)
+                    .setSubject(user.getPhoneNumber())
+                    .setExpiration(new Date(System.currentTimeMillis() + expiration * 1000L))
+                    .signWith()
+                    .compact();
+        }
+        catch (Exception e) {
+
+        }
+    }
+
+    private Key getSignInKey() {
+
+    }
+
 }
