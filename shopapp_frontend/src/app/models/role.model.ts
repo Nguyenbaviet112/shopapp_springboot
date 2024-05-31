1922c7bb9da5d94de7f8964434222cd73335d50f
Nguyenbaviet112 <nguyenbaviet0339705277@gmail.com>
2024-05-31 10:00:30 +0700

    rename environment

--- shopapp_frontend/src/app/models/role.model.ts
+++ shopapp_frontend/src/app/models/role.model.ts
@@ -1,6 +1,4 @@
-
 export interface Role {
     id: number;
-    name: String;
-    
+    name: string;
 }
--- shopapp_frontend/src/app/services/role.service.ts
+++ shopapp_frontend/src/app/services/role.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../enviroments/enviroment';
-
+import { environment } from '../environments/environment';
+@Injectable({
+  providedIn: 'root'
+})
 export class RoleService {
-    private apiGetRoles = `${environment.apiBaseUrl}/roles`;
+  private apiGetRoles  = `${environment.apiBaseUrl}/roles`;
 
-    constructor(private http: HttpClient) {}
-
-    getRoles(): Observable<any> {
-        return this.http.get<any[]>(this.apiGetRoles);
-    }
+  constructor(private http: HttpClient) { }
+  getRoles():Observable<any> {
+    return this.http.get<any[]>(this.apiGetRoles);
+  }
 }
